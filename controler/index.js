7bc3cc6d298074db8003b82b3505dd9f95aac6a8
guilhermegmc <guilhermegmcpro@gmail.com>
2022-09-14 17:11:45 -0300

    First commit

---
+++ controler/index.js
@@ -0,0 +1,11 @@
+const navbar = (
+    <nav>
+        <h1>Bob's Restaurant
+            <li>Location</li>
+            <li>Menu</li>
+            <li>Contact</li>
+        </h1>
+    </nav>
+)
+
+ReactDOM.render(navbar,document.getElementById("root"))
